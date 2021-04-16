1d7dfa207b42bcd4b234ca470c886ce343d4474e
Andre Pereira <64938482+DECO190@users.noreply.github.com>
2021-04-16 18:02:35 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,21 +1,11 @@
-let options = {
-    client: 'pg',
-    connection: 'postgres://kqbskseeiqjfmg:18d840694a5f2bd0844fe8e044bce203f352f7e0adadb9451c6d49bcbed477ed@ec2-18-206-20-102.compute-1.amazonaws.com:5432/db42ua99bg3803',
-    pool: {
-      min: 2,
-      max: 10
-    },
-}
-
-var db = require('knex')(options);
-
-// var knex = require('knex')({
-//     client: 'mysql',
-//     connection: {
-//       user : 'sql10406099',
-//       password : 'tcew7ib2xi',
-//       database : 'sql10406099'
-//     }
-// });
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+		host: '35.223.135.208',
+     	user : 'root',
+      	password : 'sempre190',
+      	database : 'decode'
+    }
+});
 
 module.exports = db
